1ae22cb44bf356b6f1aa702d105552bf2aacc714
NerokArtworks <nerokprodscontact@gmail.com>
2022-11-14 13:02:43 +0100

    NavBar Component

--- framework-php-bootstrap/includes/navigation.php
+++ framework-php-bootstrap/includes/navigation.php
@@ -1,15 +1,31 @@
 <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="index.php">Start Bootstrap</a>
+        <!-- <a class="navbar-brand" href="index.php">Start Bootstrap</a>
         <button class="navbar-toggler text-uppercase font-weight-bold text-white rounded" type="button"
             data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
             aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-
-            <ul class="navbar-nav ms-auto navbar-botones">
+        </button> -->
+        <div class="collapse navbar-collapse row justify-content-between align-items-center" id="navbarResponsive">
+            <div class="col-4">
+                <ul class="navbar-nav navbar-botones justify-content-center gap-5">
+                    <li class="nav-item"><a href="">Conciertos</a></li>
+                    <li class="nav-item"><a href="">Festivales</a></li>
+                    <li class="nav-item"><a href="">Merch</a></li>
+                </ul>
+            </div>
+            <div class="col-auto text-center">
+                <a class="navbar-brand p-0 m-0" href="index.php">Start Bootstrap</a>
+            </div>
+            <div class="col-4">
+                <ul class="navbar-nav navbar-botones justify-content-center gap-5">
+                    <li class="nav-item"><a href="">Galería</a></li>
+                    <li class="nav-item"><a href="">Términos</a></li>
+                    <li class="nav-item"><a href="">Contacto</a></li>
+                </ul>
+            </div>
+            <!-- <ul class="navbar-nav ms-auto navbar-botones">
                 <li class="nav-item dropdown">
                     <a class="nav-link py-3 px-0 px-lg-3  dropdown-toggle" href="#" id="navbarDropdown" role="button"
                         data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -30,7 +46,7 @@
                 <li class="nav-item mx-0 mx-lg-1"><a
                         class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?> py-3 px-0 px-lg-3 rounded"
                         href="contact.php">Contact</a></li>
-            </ul>
+            </ul> -->
         </div>
     </div>
 </nav>
