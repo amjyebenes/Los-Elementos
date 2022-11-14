847337b8ac26f1456be5f77eb52fc34aaf484f48
adrialhama <adrialhama93@gmail.com>
2022-11-14 13:11:36 +0100

    add footer

--- framework-php-bootstrap/includes/footer.php
+++ framework-php-bootstrap/includes/footer.php
@@ -1,33 +1,30 @@
 <footer class="footer text-center">
         <div class="container">
-            <div class="row">
-                <!-- Footer Location-->
-                <div class="col-lg-4 mb-5 mb-lg-0">
-                    <h4 class="text-uppercase mb-4">Location</h4>
-                    <p class="lead mb-0">
-                        2215 John Daniel Drive
-                        <br />
-                        Clark, MO 65243
-                    </p>
+            <div class="row justify-content-between align-items-center">
+                <!-- Footer Logo-->
+                <div class="col-lg-2 col-md-3 col-sm-4 col-4">
+                    <div class="w-75">
+                        <img src="assets/img/portfolio/Logo-web00.png" class="img-fluid">
+                    </div>
                 </div>
-                <!-- Footer Social Icons-->
-                <div class="col-lg-4 mb-5 mb-lg-0">
-                    <h4 class="text-uppercase mb-4">Around the Web</h4>
-                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i
-                            class="fab fa-fw fa-facebook-f"></i></a>
-                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
-                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i
-                            class="fab fa-fw fa-linkedin-in"></i></a>
-                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
-                </div>
-                <!-- Footer About Text-->
-                <div class="col-lg-4">
-                    <h4 class="text-uppercase mb-4">About Freelancer</h4>
-                    <p class="lead mb-0">
-                        Freelance is a free to use, MIT licensed Bootstrap theme created by
-                        <a href="http://startbootstrap.com">Start Bootstrap</a>
-                        .
-                    </p>
+              
+                <!-- Footer Contacto-->
+                <div class="col-lg-6 col-sm-8 col-8 d-flex gap-2 justify-content-end align-items-center">
+                    <span>
+                        <p class="m-0 pe-3">© Copyrigth - The Elements - 2022</p>
+                    </span>
+                    <span>                        
+                        <p class="m-0">Contacto +34 922 928 789</p>
+                    </span>
+                    <span>
+                        <i class="fa-brands fa-twitter text-white"></i>
+                    </span>
+                    <span>
+                        <i class="fa-brands fa-facebook text-white"></i>
+                    </span>
+                    <span>
+                        <i class="fa-brands fa-github text-white"></i>
+                    </span>
                 </div>
             </div>
         </div>
