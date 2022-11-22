0a03026273fba8baff2a6b3bd463243df91c0e9b
MaritaSicilia <90844940+MaritaSicilia@users.noreply.github.com>
2022-11-22 13:02:21 +0100

    Login cogiendo forma

--- framework-php-bootstrap/login.php
+++ framework-php-bootstrap/login.php
@@ -12,17 +12,19 @@
         <!--Creo la caja grande para almacenar el resto  -->
         <section class="page-section">
            <!-- Creo el div donde meteremos el contenedor que almacena el formulario--> 
-            <div class="container">
-            <!--Meto el icono fontawesome-->
+            <div class="container d-flex justify-content-center">
+            <div class="row col-6 ">
             <form action="" method="POST">
-                    <div class="d-flex justify-content-center mt-5 ">
-                        <i class="fa-solid fa-user-group fa-6x"></i>
+                    <div class="d-flex justify-content-center mt-5 mb-5">
+                        <i class="fa-solid fa-user-group fa-6x iconoLogin"></i>
                     </div>
-                    <div class="mb-3 mt-3">
+                    <div class="mb-3 ">
                         <input type="email" class="form-control" id="email" placeholder="Email" name="email">
                     </div>
                     <div class="mb-3">                    
                         <input type="password" class="form-control" id="pwd" placeholder="Contraseña" name="pass">
+                       <!-- Esto es el icono del ojo de la contraseña
+                        <i class="fa-regular fa-eye-slash"></i> -->
                     </div>
                     <div class="form-check mb-3">
                         <label class="form-check-label text-white">
@@ -30,7 +32,7 @@
                         </label>
                     </div>
                     <div class="mb-3">
-                        <button type="submit" class="btn btn-outline-primary bg-secondary text-white">Iniciar Sesión</button>
+                        <button type="submit" class="btn btn-outline-primary bg-secondary text-white rounded-3">Iniciar Sesión</button>
                         <p type="button" class="text-white">¿Olvidaste tu contraseña?</p>
                     </div>
                     <div class="mb-3">
@@ -41,6 +43,8 @@
                     </div>
                     
                 </form>
+            </div>
+
             </div>
         </section>
 
