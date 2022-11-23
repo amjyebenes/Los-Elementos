ca33a102a98addcbbe17f60e519621ae23aee165
angel2 <angelote2229914@gmail.com>
2022-11-23 14:58:40 +0100

    cambios micuenta.php

--- framework-php-bootstrap/micuenta.php
+++ framework-php-bootstrap/micuenta.php
@@ -9,40 +9,60 @@
 <?php include("includes/navigation.php");?>
 <main class="my-5">
 
-    <div class="container pt-4">
-        <div class="row pt-4">
-            <h6 class="bg-primary p-1 ps-4"><span class="text-white shadow">MI&nbsp&nbspZONA</span></h6>
-        </div>
-        <div class="row mt-4 pt-4 d-flex justify-content-center p-3">
-            <div class="col ms-4 me-5 mb-4 d-flex cajaMiCuenta rounded-3 ">
-                <div class="row col justify-content-center text-center ">
-                    <i class="fas fa-user fa-7x iconoCuenta mt-3 w100"></i>
-                    <span class="text-primary mt-2 mb-3 h5 tituloArtista">Mis datos</span>
-                </div>
-            </div>
-            <div class="col ms-4 me-5 mb-4 d-flex cajaMiCuenta rounded-3">
-                <div class="row col justify-content-center text-center">
-                    <i class="fas fa-truck fa-6x iconoCuenta mt-3 w100"></i>
-                    <span class="text-primary mt-2 mb-3 h5 tituloArtista">Direccion de envio</span>
-                </div>
-            </div>
-            <div class="col ms-4 me-5 mb-4 d-flex cajaMiCuenta rounded-3">
-                <div class="row col justify-content-center text-center">
-                    <i class="fa-solid fa-basket-shopping fa-5x iconoCuenta mt-4 w-100"></i>
-                    <span class="text-primary mt-2 mb-3 h5 tituloArtista">Mis compras</span>
-                </div>
-            </div>
-            <div class="col ms-4 me-5 mb-4 d-flex cajaMiCuenta rounded-3">
-                <div class="row col justify-content-center text-center">
-                    <i class="fa-solid fa-bell fa-5x iconoCuenta mt-4 w100"></i>
-                    <span class="text-primary mt-2 mb-3 h5 tituloArtista">Notificaciones</span>
-                </div>
-            </div>
-        </div>
+    <div class="container pt-4 ">
+        <div class="row col pt-4 justify-content-center">
+            <h6 class="col col-md-10 col-lg-8 bg-primary p-1 ps-4"><span class="text-white shadow">MI&nbsp&nbspZONA</span></h6>
         
-        <div class="nav navbar container">
-            <div class="nav-lineOrange position-relative bottom-0"></div>
+            <div class="row mt-4 pt-4 d-flex justify-content-center">
+                <div class="col col-lg-3 ms-5 me-5 mb-4 d-flex cajaMiCuenta rounded-3 ">
+                    <div class="row col justify-content-center text-center ">
+                        <i class="fas fa-user fa-7x iconoCuenta mt-3 w100"></i>
+                        <span class="text-primary mt-2 mb-3 h5 tituloArtista">Mis datos</span>
+                    </div>
+                </div>
+                <div class="col col-lg-3 ms-5 me-5 mb-4 d-flex cajaMiCuenta rounded-3">
+                    <div class="row col justify-content-center text-center">
+                        <i class="fa-solid fa-basket-shopping fa-5x iconoCuenta mt-4 w-100"></i>
+                        <span class="text-primary mt-2 mb-3 h5 tituloArtista">Mis compras</span>
+                    </div>
+                </div>
         </div>
+
+        <div class="nav nav-lineOrange container pt-4 justify-content-center">
+            <div class="row col col-md-10 col-lg-8 bg-primary p-1 ps-4">
+            </div>
+        </div>
+
+        <div class="container pt-4 ">
+            <div class="row col pt-4 justify-content-center">
+                <div class="col col-md-10 col-lg-8 row">
+                    <div class="row col-sm-6">
+                        <p class="h5 tituloArtista textoNaranja"><u>DATOS DE FACTURACION</u></p>
+                        <div class="col">
+                            <p class="h6 tituloArtista text-left fw-bold">NOMBRE: </p>
+                            <p class="h6 tituloArtista text-left fw-bold">DNI/CIF: </p>
+                            <p class="h6 tituloArtista text-left fw-bold">TELEFONO: </p>
+                            <p class="h6 tituloArtista text-left fw-bold">DIRECCION: </p>
+                        </div>
+                        <div class="col">
+                            <p class="h6 tituloArtista text-rigth">juan</p>
+                            <p class="h6 tituloArtista text-rigth">25146463s</p>
+                            <p class="h6 tituloArtista text-rigth">95544115</p>
+                            <p class="h6 tituloArtista text-rigth">c/las pilas</p>
+                        </div>
+                    </div>
+                    <div class="row col-sm-6">
+                        <p class="h5 tituloArtista textoNaranja"><u>DATOS DE CUENTA</u></p>
+                    </div>
+                    
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
+    
+    
 
     </div>
 
