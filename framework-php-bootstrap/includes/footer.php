2430b168eeeb992d91a7b16dfeae46fdb8e58348
adrialhama <adrialhama93@gmail.com>
2022-11-24 21:27:21 +0100

    mod-footerFinal

--- framework-php-bootstrap/includes/footer.php
+++ framework-php-bootstrap/includes/footer.php
@@ -2,27 +2,27 @@
         <div class="container">
             <div class="row">
                 <!-- Footer Logo-->
-                <div class="col-lg-3 col-md-3 col-sm-3 col-12 d-flex justify-content-center align-items-center">
+                <div class="col-lg-3 col-sm-6 col-8 d-flex justify-content-end justify-content-sm-start align-items-center pb-2 pb-sm-0">
                     <div class="w-50">
                         <img src="assets/img/Logo-web00.png" class="img-fluid">
                     </div>
                 </div>
               
                 <!-- Footer Contacto-->
-                <div class="col-lg-4 col-sm-4 col-12 d-flex justify-content-center justify-content-sm-end align-items-center ms-lg-5">
+                <div class="col-lg-4 col-sm-6 col-12 d-flex justify-content-center justify-content-sm-end align-items-center ms-lg-5">
                     <span>
                         <p class="m-0">© Copyright - The Elements - 2022</p>
                     </span>
                 </div>
 
-                <div class="col-lg-4 col-sm-5 col-12 d-flex gap-4 justify-content-center justify-content-sm-end align-items-center">
+                <div class="col-lg-4 col-sm-12 col-12 d-flex gap-4 justify-content-center justify-content-sm-end align-items-center">
                     <span>                        
                         <p class="m-0">Contacto +34 922 928 789</p>
                     </span>
                     <span class="gap-2">
-                        <i class="fa-brands fa-twitter text-white"></i>
-                        <i class="fa-brands fa-facebook text-white"></i>
-                        <i class="fa-brands fa-github text-white"></i>
+                        <i class="fa-brands fa-twitter text-white fa-lg"></i>
+                        <i class="fa-brands fa-facebook text-white fa-lg"></i>
+                        <i class="fa-brands fa-github text-white fa-lg"></i>
                     </span>
                 </div>
             </div>
