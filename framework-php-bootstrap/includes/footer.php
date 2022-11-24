974eedcc77a6f7785e75083afc0a1554387d35a2
adrialhama <adrialhama93@gmail.com>
2022-11-24 21:09:02 +0100

    modificado footer

--- framework-php-bootstrap/includes/footer.php
+++ framework-php-bootstrap/includes/footer.php
@@ -1,22 +1,25 @@
 <footer class="footer text-center">
         <div class="container">
-            <div class="row justify-content-between align-items-center">
+            <div class="row">
                 <!-- Footer Logo-->
-                <div class="col-lg-2 col-md-3 col-sm-4 col-4">
-                    <div class="w-75">
+                <div class="col-lg-3 col-md-3 col-sm-3 col-12 d-flex justify-content-center align-items-center">
+                    <div class="w-50">
                         <img src="assets/img/Logo-web00.png" class="img-fluid">
                     </div>
                 </div>
               
                 <!-- Footer Contacto-->
-                <div class="col-lg-10 col-sm-8 col-8 d-flex gap-4 justify-content-end align-items-center">
+                <div class="col-lg-4 col-sm-4 col-12 d-flex justify-content-center justify-content-sm-end align-items-center ms-lg-5">
                     <span>
-                        <p class="m-0 pe-3">© Copyright - The Elements - 2022</p>
+                        <p class="m-0">© Copyright - The Elements - 2022</p>
                     </span>
+                </div>
+
+                <div class="col-lg-4 col-sm-5 col-12 d-flex gap-4 justify-content-center justify-content-sm-end align-items-center">
                     <span>                        
                         <p class="m-0">Contacto +34 922 928 789</p>
                     </span>
-                    <span class="d-flex gap-2">
+                    <span class="gap-2">
                         <i class="fa-brands fa-twitter text-white"></i>
                         <i class="fa-brands fa-facebook text-white"></i>
                         <i class="fa-brands fa-github text-white"></i>
