a352b4da79ca64c89a18cfc31dca3b9d0d1cc927
MaritaSicilia <90844940+MaritaSicilia@users.noreply.github.com>
2022-11-25 11:02:26 +0100

    Modificacion Salas

--- framework-php-bootstrap/registro.php
+++ framework-php-bootstrap/registro.php
@@ -8,7 +8,7 @@
 <?php include("includes/navigation.php");?>
             <main class="fondoLogin pb-5">
                 <!--Creo la caja grande para almacenar el resto  -->
-                <section class="position-relative py-5 portfolio ">
+                <section class="position-relative py-5">
                 <!-- Creo el div donde meteremos el contenedor que almacena el formulario--> 
                         <div class="container d-flex justify-content-center">
                             <div class="row col-6 ">
