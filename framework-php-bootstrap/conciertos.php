2a452b0776e07b32e5be1f0efeecf7b0b797ad8d
angel2 <angelote2229914@gmail.com>
2022-11-25 14:52:34 +0100

    cambios concierto angel

--- framework-php-bootstrap/conciertos.php
+++ framework-php-bootstrap/conciertos.php
@@ -7,9 +7,9 @@
 <body>
 
 <?php include("includes/navigation.php");?>
-<main>
+<main class="device-padding">
 <!-- Portfolio Section-->
-<section class="position-relative pt-5 mt-5 portfolio mb-0" id="portfolio">
+<section class="position-relative pt-md-5 mt-md-5 portfolio mb-0" id="portfolio">
         <div class="container">
             <!-- Portfolio Section Heading-->
             <h1 class="text-left text-uppercase text-black fw-light m-0">Conciertos</h1>
@@ -28,7 +28,7 @@
                   <img class="shadow-lg card-img" src="./assets/img/cruzzi.jpg" alt="Title">
                 </div>
             </div>
-            <div class="col-8 col-md-5 d-flex flex-column">
+            <div class="col-5 col-md-5 d-flex flex-column">
                 <div class="row">
                     <p class="h2 text-primary fw-bold tituloArtista">Cruz Cafuné</p>
                     <div class="col">
@@ -41,7 +41,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-md-2 d-flex align-items-end justify-content-end mb-2">
+            <div class="col-4 col-md-2 d-flex align-items-end justify-content-end mb-2">
                 <div class="row align-items-center">
                     <div class="col-6 justify-content-center align-items-end pr-3">
                        <div class="text-center contadorEntrada">
@@ -59,7 +59,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-6 col-md-2 d-flex align-items-end mb-4">
+            <div class="col-12 col-md-2 d-flex align-items-end mb-4">
                     <div class="col" >
                         <button type="button" class="btn btn-primary rounded-3">Añadir al carrito</button>
                     </div>
