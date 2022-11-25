ec528e908a8886dad4ef4a947e711025be38b035
MaritaSicilia <90844940+MaritaSicilia@users.noreply.github.com>
2022-11-25 19:51:23 +0100

    Por ser viernes

---
+++ framework-php-bootstrap/seleccionEntradas.php
@@ -0,0 +1,29 @@
+<?php include("includes/a_config.php"); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include("includes/head-tag-contents.php"); ?>
+</head>
+
+<body>
+<?php include("includes/navigation.php");?>
+<main class="py-5 container-fluid">
+
+    <section class="position-relative mt-5 mb-0">
+            <div class="container">
+                <h1 class="text-center text-uppercase text-black m-0">Juan Tamariz - 24 Junio - Cines Callao </h1>
+            </div>
+            <div class="nav navbar container">
+                <div class="nav-line position-relative bottom-0"></div>
+            </div>
+    </section>
+    <section>
+        <div class="container fluid">
+            
+        </div>
+    </section>
+</main>
+
+</body>
+</html>
