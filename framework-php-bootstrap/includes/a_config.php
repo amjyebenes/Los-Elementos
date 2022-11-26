d1f6316ca18ac7bcbc67e5e3e8a43dad62004b78
Ángel Manuel Joaquín Yébenes <amjyebenes@gmail.com>
2022-11-26 19:51:19 +0100

    cambios cuenta y conciertos y titulos de paginas angel

--- framework-php-bootstrap/includes/a_config.php
+++ framework-php-bootstrap/includes/a_config.php
@@ -2,18 +2,42 @@
 	switch ($_SERVER["SCRIPT_NAME"]) {
 		case "/index.php":
 			$CURRENT_PAGE = "Index"; 
-			$PAGE_TITLE = "Portfolio";
+			$PAGE_TITLE = "Home - EleTickets";
 			break;
-		case "/about.php":
-			$CURRENT_PAGE = "About"; 
-			$PAGE_TITLE = "About Us";
+		case "/cesta.php":
+			$CURRENT_PAGE = "Cesta"; 
+			$PAGE_TITLE = "Cesta - EleTickets";
 			break;
 		case "/contact.php":
-			$CURRENT_PAGE = "Contact"; 
-			$PAGE_TITLE = "Contact Us";
+			$CURRENT_PAGE = "Contacto"; 
+			$PAGE_TITLE = "Contactar - EleTickets";
+			break;
+		case "/login.php":
+			$CURRENT_PAGE = "Login"; 
+			$PAGE_TITLE = "Iniciar Sesion - EleTickets";
+			break;
+		case "/micuenta.php":
+			$CURRENT_PAGE = "Mi cuenta"; 
+			$PAGE_TITLE = "Mi cuenta - EleTickets";
+			break;
+		case "/registro.php":
+			$CURRENT_PAGE = "Registro"; 
+			$PAGE_TITLE = "Registro - EleTickets";
+			break;
+		case "/salas.php":
+			$CURRENT_PAGE = "Salas"; 
+			$PAGE_TITLE = "Salas - EleTickets";
+			break;
+		case "/conciertos.php":
+			$CURRENT_PAGE = "Conciertos"; 
+			$PAGE_TITLE = "Conciertos - EleTickets";
+			break;
+		case "/terminosYcondiciones.php":
+			$CURRENT_PAGE = "Términos y Condiciones"; 
+			$PAGE_TITLE = "Términos y Condiciones - EleTickets";
 			break;
 		default:
 			$CURRENT_PAGE = "Index";
-			$PAGE_TITLE = "Welcome to my homepage!";
+			$PAGE_TITLE = "No te quedes sin tus entradas - EleTickets";
 	}
 ?>
