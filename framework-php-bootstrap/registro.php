07ad1cd5dcb9e358f18e15b1ca3afa0b19d250ce
adrialhama <adrialhama93@gmail.com>
2022-11-28 10:53:28 +0100

    arreglado

--- framework-php-bootstrap/registro.php
+++ framework-php-bootstrap/registro.php
@@ -11,10 +11,10 @@
                 <section class="position-relative py-5">
                 <!-- Creo el div donde meteremos el contenedor que almacena el formulario--> 
                         <div class="container d-flex justify-content-center pt-5">
-                            <div class="row col-6 ">
+                            <div class="row col-md-6 col-12">
                                 <form action="" method="POST">
                                 <div class="d-flex justify-content-center mt-5 mb-5">
-                                    <i class="fa-solid fa-user-plus fa-6x iconoLogin"></i>                                    
+                                    <i class="fa-solid fa-user-plus fa-6x text-info"></i>                                    
                                 </div> 
                                 <div class="d-flex align-items-center gap-2 mb-3">
                                     <i class="fa-solid fa-ticket text-white"></i>
