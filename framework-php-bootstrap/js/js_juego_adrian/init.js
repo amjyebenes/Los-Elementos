aafdfdbdc7a25e7df55299a8a515cb0dd94ce9a5
adrialhama <adrialhama93@gmail.com>
2023-01-20 11:30:55 +0100

    mod juego

--- framework-php-bootstrap/js/js_juego_adrian/init.js
+++ framework-php-bootstrap/js/js_juego_adrian/init.js
@@ -2,8 +2,8 @@ import Bootloader from './bootloader.js';
 import Scene_play from './scenes/scene_play.js';
 import Scene_end from './scenes/scene_end.js';
 const config = {
-    width: 640,
-    height: 400,
+    width: 740,
+    height: 500,
     parent: "contenedor-pong", // El parent debe ser con un id del HTML de lo contrario puede producir errores
     type: Phaser.AUTO, /*En auto el navegador puede coger canvas o webgl.*/
     physics: { // El tipo de fisicas del juego
