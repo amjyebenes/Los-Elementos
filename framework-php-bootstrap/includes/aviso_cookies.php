6fed8d32d71b72175bffc3b93f45fc580218f70d
angel <amjyebenes@gmail.com>
2023-02-21 11:28:37 +0100

    cambios color

--- framework-php-bootstrap/includes/aviso_cookies.php
+++ framework-php-bootstrap/includes/aviso_cookies.php
@@ -2,7 +2,7 @@
         <div class='cookie-content'>
             <p class="fw-bold">Este sitio utiliza cookies para mejorar tu experiencia. Asumiré que estás de acuerdo con esto.</p>
             <div class='buttons d-flex justify-content-center align-items-center gap-4 '>
-                <button class='item btn btn-secondary rounded-3 text-white'>Acepto</button>
+                <button class='item btn btn-secondary rounded-3 text-dark'>Acepto</button>
                 <a class='item text-white shadowText' href='cookies.php' target='_blank'>Leer más</a>
             </div>
         </div>
